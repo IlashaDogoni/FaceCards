1fb880f45258e530f78e0f297bbd64f1fbd9289b
IlashaDogoni <fistonfire@mail.ru>
2022-05-05 18:28:36 +0600

    Initial Commit

---
+++ FaceCards/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  FaceCards
+//
+//  Created by Ilya Kokorin on 05.05.2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
